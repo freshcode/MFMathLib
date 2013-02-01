ecb88eb33878a68886798ebf0ec2f5836c172bc8
Dave Poirier <ekscrypto@gmail.com>
2013-02-01 00:22:02 -0500

    Initial Commit

---
+++ MathLib/MF_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MF_AppDelegate.h
+//  MathLib
+//
+//  Created by Dave Poirier on 2013-02-01.
+//  Copyright (c) 2013 Freshcode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MF_AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MathLib/MF_AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MF_AppDelegate.m
+//  MathLib
+//
+//  Created by Dave Poirier on 2013-02-01.
+//  Copyright (c) 2013 Freshcode. All rights reserved.
+//
+
+#import "MF_AppDelegate.h"
+
+@implementation MF_AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
